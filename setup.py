c7e2e2ec3a13808b4508d79acf386d46f53abbb2
Jeff Forcier <jeff@bitprophet.org>
2013-09-15 20:55:52 -0700

    Initial import of ported-from-Fabric code.
    Has config stuff added on top, otherwise picks up
    from Fabric 7084464a71ac79ae0bf940f394fa4f08f771355a

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='releases',
+    version='0.2.0',
+    description='A Sphinx extension for changelog manipulation',
+    author='Jeff Forcier',
+    author_email='jeff@bitprophet.org',
+    url='https://github.com/bitprophet/releases',
+    packages=['releases'],
+    classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Unix',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Build Tools',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
