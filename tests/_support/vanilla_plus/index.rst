89523705f2fa6c9f529bd138f0d5b818050b2e9c
Jeff Forcier <jeff@bitprophet.org>
2014-04-06 10:45:34 -0700

    Other-files tests are just dumb, don't bother

--- tests/_support/vanilla_plus/index.rst
+++
@@ -1,7 +0,0 @@
-====
-Test
-====
-
-.. toctree::
-    changelog
-    other
--- tests/integration.py
+++ tests/integration.py
@@ -41,10 +41,6 @@ class integration(Spec):
         # Doctree with just-a-changelog-named-changelog
         self._assert_worked('vanilla')
 
-    def vanilla_with_other_files(self):
-        # Same but w/ other files in toctree
-        self._assert_worked('vanilla_plus')
-
     def customized_filename_with_identical_title(self):
         # Changelog named not 'changelog', same title
         self._assert_worked(
@@ -61,10 +57,6 @@ class integration(Spec):
             opts={'releases_document_name': 'notachangelog'},
             target='notachangelog',
         )
-
-    def customized_filename_with_other_files(self):
-        # Same as above but w/ other files in toctree
-        skip()
 
     def useful_error_if_changelog_is_missed(self):
         # Don't barf with unknown-node errors if changelog not found.
