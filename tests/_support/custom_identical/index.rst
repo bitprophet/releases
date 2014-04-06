25819952d1260dfe7c47a0e078ca15212be96d6f
Jeff Forcier <jeff@bitprophet.org>
2014-04-06 10:41:11 -0700

    Custom filename test

---
+++ tests/_support/custom_identical/index.rst
@@ -0,0 +1,6 @@
+====
+Test
+====
+
+.. toctree::
+    notachangelog
--- tests/integration.py
+++ tests/integration.py
@@ -12,15 +12,24 @@ class integration(Spec):
     def teardown(self):
         os.chdir(self.cwd)
 
-    def _assert_worked(self, folder):
+    def _assert_worked(self, folder, opts=None, target='changelog'):
+        # Dynamic sphinx opt overrides
+        pairs = map(lambda x: '='.join(x), (opts or {}).items())
+        flags = map(lambda x: '-D {0}'.format(x), pairs)
+        flagstr = ' '.join(flags)
+        # Setup
         os.chdir(os.path.join('tests', '_support'))
         build = os.path.join(folder, '_build')
         try:
-            cmd = 'sphinx-build -c . -W {0} {1}'.format(folder, build)
+            # Build
+            cmd = 'sphinx-build {2} -c . -W {0} {1}'.format(
+                folder, build, flagstr)
             result = run(cmd, warn=True, hide=True)
+            # Check for errors
             msg = "Build failed w/ stderr: {0}"
             assert result.ok, msg.format(result.stderr)
-            changelog = os.path.join(build, 'changelog.html')
+            # Check for vaguely correct output
+            changelog = os.path.join(build, '{0}.html'.format(target))
             with open(changelog) as fd:
                 text = fd.read()
                 assert "1.0.1" in text
@@ -38,7 +47,11 @@ class integration(Spec):
 
     def customized_filename_with_identical_title(self):
         # Changelog named not 'changelog', same title
-        skip()
+        self._assert_worked(
+            folder='custom_identical',
+            opts={'releases_document_name': 'notachangelog'},
+            target='notachangelog',
+        )
 
     def customized_filename_with_different_title(self):
         # Changelog named not 'changelog', title distinct
