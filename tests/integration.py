ac901178d5d371e412cf89b019970d558178e234
Jeff Forcier <jeff@bitprophet.org>
2014-04-06 10:01:56 -0700

    Move conf.py up a level as it's same for all tests

--- tests/integration.py
+++ tests/integration.py
@@ -14,17 +14,17 @@ class integration(Spec):
 
     def vanilla_invocation(self):
         # Doctree with just-a-changelog-named-changelog
-        os.chdir('tests/_support/vanilla')
+        os.chdir('tests/_support')
         try:
-            result = run('sphinx-build -W . _build', warn=True, hide=True)
+            result = run('sphinx-build -c . -W vanilla vanilla/_build', warn=True, hide=True)
             msg = "Build failed w/ stderr: {0}"
             assert result.ok, msg.format(result.stderr)
-            with open('_build/changelog.html') as fd:
+            with open('vanilla/_build/changelog.html') as fd:
                 text = fd.read()
                 assert "1.0.1" in text
                 assert "#1" in text
         finally:
-            shutil.rmtree('_build')
+            shutil.rmtree('vanilla/_build')
 
     def vanilla_with_other_files(self):
         # Same but w/ other files in toctree
