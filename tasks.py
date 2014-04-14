8a56a031ee30f04c7a3f2583d214dca9b7601e9d
Dorian Pula <dorian.pula@amber-penguin-software.ca>
2014-04-14 17:52:24 -0400

    Add integration test task.

--- tasks.py
+++ tasks.py
@@ -3,5 +3,14 @@ from invocations.testing import test
 from invocations.packaging import release
 
 from invoke import Collection
+from invoke import run
+from invoke import task
 
-ns = Collection(test, release, docs)
+
+@task()
+def integration_tests():
+    """Runs integration tests."""
+    run('inv test -o --tests=integration')
+
+
+ns = Collection(test, integration_tests, release, docs)
