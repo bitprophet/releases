4f9bb7a81f52b5ee46be338e5c699411286f1401
Jeff Forcier <jeff@bitprophet.org>
2015-11-05 11:48:44 -0800

    Rename integration tests task for consistency w/ other projs

--- tasks.py
+++ tasks.py
@@ -10,10 +10,10 @@ from invoke import task
 @task(help={
     'pty': "Whether to run tests under a pseudo-tty",
 })
-def integration_tests(pty=True):
+def integration(pty=True):
     """Runs integration tests."""
     cmd = 'inv test -o --tests=integration'
     run(cmd + ('' if pty else ' --no-pty'), pty=pty)
 
 
-ns = Collection(test, integration_tests, release, docs)
+ns = Collection(test, integration, release, docs)
