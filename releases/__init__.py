386d58d07deabaecefd853a2a4b6013284fdfe69
Jeff Forcier <jeff@bitprophet.org>
2016-04-27 21:32:45 -0700

    Fix first failing presentation bug

--- releases/__init__.py
+++ releases/__init__.py
@@ -165,8 +165,10 @@ def append_unreleased_entries(app, lines, releases):
     for family, _lines in six.iteritems(lines):
         for type_ in ('bugfix', 'feature'):
             issues =  _lines['unreleased_{0}'.format(type_)]
+            fam_prefix = "{0} ".format(family) if len(lines) > 1 else ""
+            header = "Next {0}{1} release".format(fam_prefix, type_)
             nodelist = [release_nodes(
-                "Next {0} {1} release".format(family, type_),
+                header,
                 # TODO: should link to master for newest family and...what
                 # exactly, for the others? Expectation isn't necessarily to
                 # have a branch per family? Or is there? Maybe there must be..
